9f38ee28f731b347ec1a51061ffb145df141874c
Keith James <keith@zen.(none)>
2007-11-29 17:44:12 +0000

    Added array subscript type

--- src/cl-gp-utilities.lisp
+++ src/cl-gp-utilities.lisp
@@ -1,14 +1,18 @@
 
 (in-package :cl-gp-utilities)
 
+(deftype array-subscript ()
+  '(and fixnum (integer 0 *)))
+
 (defmacro copy-array (source source-start source-end
                       dest dest-start &optional key)
   "Copies elements SOURCE indices SOURCE-START and SOURCE-END to DEST,
 inserting them into DEST at DEST-START onwards. If the function KEY is
 supplied, it is applied to each element of SOURCE prior to its
 insertion into DEST."
-  `(loop for si of-type fixnum from ,source-start to ,source-end
-      for di of-type fixnum = ,dest-start then (1+ di)
+  `(loop for si of-type array-subscript from ,source-start to ,source-end
+      for di of-type array-subscript = ,dest-start
+      then (the array-subscript (1+ di))
       do (setf (aref ,dest di) ,(if key
                                     `(funcall ,key (aref ,source si))
                                   `(aref ,source si)))))
