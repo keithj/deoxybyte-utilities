300ff515fbb270c2608007e95c6d017c6cb7dc20
Keith James <dev@deoxybyte.co.uk>
2009-08-17 16:24:19 +0100

    * Added some commonly used deftypes. Exported their symbols.
    * Bumped revision.

---
+++ src/type-utilities.lisp
@@ -0,0 +1,48 @@
+;;;
+;;; Copyright (C) 2009 Keith James. All rights reserved.
+;;;
+;;; This file is part of deoxybyte-utilities.
+;;;
+;;; This program is free software: you can redistribute it and/or modify
+;;; it under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation, either version 3 of the License, or
+;;; (at your option) any later version.
+;;;
+;;; This program is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+;;;
+
+(in-package :uk.co.deoxybyte-utilities)
+
+(deftype uint8 ()
+  "Alias for unsigned-byte 8."
+  '(unsigned-byte 8))
+
+(deftype int8 ()
+  "Alias for signed-byte 8."
+  '(signed-byte 8))
+
+(deftype uint16 ()
+  "Alias for unsigned-byte 16."
+  '(unsigned-byte 16))
+
+(deftype int16 ()
+  "Alias for signed-byte 16."
+  '(signed-byte 16))
+
+(deftype uint32 ()
+  "Alias for unsigned-byte 32."
+  '(signed-byte 32))
+
+(deftype int32 ()
+  "Alias for signed-byte 32."
+  '(signed-byte 32))
+
+(deftype array-index ()
+  "Array index type."
+  '(and fixnum (integer 0 *)))
